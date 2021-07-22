ba523cf8e28ea3ae120b47e8b2bdb4e135c1130e
Caleb Babin <object.caleb@gmail.com>
2021-07-22 14:43:37 -0500

    update to angular 11 (broken)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,10 +6,11 @@ import { routes } from "./routes";
 @NgModule({
     imports: [
         RouterModule.forRoot(routes, {
-            scrollPositionRestoration: 'enabled',
-            anchorScrolling: 'enabled',
-            scrollOffset: [0, 64]
-        })
+    scrollPositionRestoration: 'enabled',
+    anchorScrolling: 'enabled',
+    scrollOffset: [0, 64],
+    relativeLinkResolution: 'legacy'
+})
     ], exports: [RouterModule]
 })
 export class AppRoutingModule {
